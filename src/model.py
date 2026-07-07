1929a0e58146e21a114c19e6d96c5a4eb4d24fa3
Cadence Centers <cadencecenters2@gmail.com>
2026-07-07 11:01:47 -0500

    Implemented the Autoencoder class

---
+++ src/model.py
@@ -0,0 +1,29 @@
+# src/model.py
+import torch
+import torch.nn as nn
+
+class Autoencoder(nn.Module):
+    def __init__(self, latent_dim=32): # Default latant space dimension is 32
+        super(Autoencoder, self).__init__()
+        
+        # Encoder: Compresses 784 pixels down to latent_dim
+        self.encoder = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, latent_dim),
+            nn.ReLU()
+        )
+        
+        # Decoder: Expands latent_dim back to 784 pixels
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 28 * 28),
+            nn.Sigmoid() # Sigmoid forces output to [0, 1] range
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded.view(-1, 1, 28, 28) # Reshape back to image format
